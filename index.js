5118f1f8200a8d722dd7d7c3cc54eca5f15fd2d2
nodeAriful <ariful.node82@gmail.com>
2021-01-30 22:23:21 +0600

    usig method as arrow=()=>{} function

--- index.js
+++ index.js
@@ -10,12 +10,33 @@ class Person {
         this.name = name
         this.email = email
     }
+
+    changeName(name) {
+        this.name = name
+    }
+    arrow = (email) => {
+        this.email = email;
+    }
+    sendEmail(msg) {
+        console.log('To', this.email);
+        console.log('Sending Email', msg);
+    }
+
+    print() {
+        console.log(this);
+    }
 }
 
 const p1 = new Person('Ariful Islam', 'arifulislam@gmail.com');
-const p2 = new Person('Aysha Moni','ayshamoni@gmail.com');
-const p3 = new Person('Zarif', 'zarif@emil.com')
+const p2 = new Person('Aysha Moni', 'ayshamoni@gmail.com');
+const p3 = new Person('Zarif', 'zarif@email.com')
 
-console.log(p1);
-console.log(p2);
-console.log(p3);
+p3.sendEmail('Hello')
+
+p1.changeName('M.A.I. Raju')
+
+p1.print()
+p3.print()
+
+p1.arrow('ariful4082@gmail.com')
+p1.print();
