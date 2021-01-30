fda928a72bc95220bec61927f5dcc5c23862d832
nodeAriful <ariful.node82@gmail.com>
2021-01-30 21:39:46 +0600

    simple person create to useing class

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const person = {};
 person.name = 'Ariful Islam';
 person.email= 'ariful4082@gmail.com';
 console.log(person);
+
+class Person {
+
+}
+const p1 = new Person();
+p1.name = 'Person 1';
+p1.email = 'person@gmail.com';
+
+console.log(p1);
+
+const p2 = new Person();
+p2.name = 'Person 2';
+p2.email = 'person2@gmail.com';
+
+console.log(p2);
