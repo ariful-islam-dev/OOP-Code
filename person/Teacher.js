bc605e6c05d511fc6fc288e3cfba08337fecef60
nodeAriful <ariful.node82@gmail.com>
2021-02-02 00:53:45 +0600

    created Teacher Class

---
+++ person/Teacher.js
@@ -0,0 +1,25 @@
+const Employeee = require('./Employee');
+
+const _subject = Symbol('subject')
+
+class Teacher extends Employeee {
+    constructor(id, name, subject) {
+        super(id, name);
+        this[_subject] = subject
+    }
+
+    get subject() {
+        return this[_subject];
+    }
+    set subject(value) {
+        this[_subject] = value
+    }
+
+    toString() {
+        return `
+        ${super.toString()}, Subject = ${this[_subject]}
+        `
+    }
+}
+
+module.exports = Employeee;
